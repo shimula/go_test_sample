e05230889694c6b2139d11855d045a298e9d562f
shimula <shimula@users.noreply.github.com>
2016-06-20 12:32:11 +0900

    initial

---
+++ hello_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGreeting(t *testing.T) {
+	expected := "hello"
+	actual := greeting()
+	if   expected != actual {
+		t.Errorf("expected is [%s], actual is [%s]", expected, actual)
+	}
+
+}
+
+
+func TestGreetingFail(t *testing.T) {
+	expected := "Hello"
+	actual := greeting()
+	if  expected != actual {
+		t.Errorf("expected is [%s], actual is [%s]", expected, actual)
+	}
+
+}
