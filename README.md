e6f891230e81500e57721e51002ae18367961adf
shimula <shimula@users.noreply.github.com>
2016-06-21 12:37:36 +0900

    test mysql services

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 [![wercker status](https://app.wercker.com/status/eba45d9a0338240289150a8186233db8/s/master "wercker status")](https://app.wercker.com/project/bykey/eba45d9a0338240289150a8186233db8)
+
+## run mysql
+
+```
+docker run -d -p 3306:3306 -v $(pwd)/test_data:/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=rootpassword -e MYSQL_USER=testuser -e MYSQL_PASSWORD=password  mysql:5.5 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+```
--- echo_main.go
+++ echo_main.go
@@ -2,15 +2,78 @@ package main
 
 import (
 	"net/http"
+	"database/sql"
+	"log"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
+	"fmt"
 )
+
+func getDB() (db *sql.DB) {
+
+	dbHost := os.Getenv("MYSQL_HOST")
+	if dbHost == "" {
+		dbHost = "192.168.99.100"
+	}
+
+	dbUser := os.Getenv("MYSQL_USER")
+	if dbUser == "" {
+		dbUser = "testuser"
+	}
+	dbPort := os.Getenv("MYSQL_PORT")
+	if dbPort == "" {
+		dbPort = "3306"
+	}
+	dbPassword := os.Getenv("MYSQL_PASSWORD")
+	if dbPassword == "" {
+		dbPassword = "password"
+	}
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/test", dbUser, dbPassword, dbHost, dbPort))
+	if err != nil {
+		log.Fatal("open erro: %v", err)
+	}
+	return db
+}
+
+func index(c echo.Context) error {
+	return c.String(http.StatusOK, "hello, world")
+}
+
+func getUsers(c echo.Context) error {
+	db := getDB()
+	defer db.Close()
+
+	id := c.Param("id")
+
+	rows, qerr := db.Query("select * from users where id = ?", id)
+
+	defer rows.Close()
+	if qerr != nil {
+		return qerr
+	}
+
+	results :=  ""
+	for rows.Next() {
+		var id string
+		if berr := rows.Scan(&id); berr != nil {
+			log.Fatal("scan erro: %v", berr)
+		}
+
+		results = results + id + "\n"
+	}
+
+	if results != "" {
+		return c.String(http.StatusOK, results)
+	}
+	return echo.ErrNotFound
+}
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Run(standard.New(":1323"))
+	e.GET("/", index)
+	e.GET("/users/:id", getUsers)
+	e.Run(standard.New(":8888"))
 }
